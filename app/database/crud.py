54e09179967586b4df13ff85e3b65a87ba226861
Dev-Umisol <148490822+Dev-Umisol@users.noreply.github.com>
2026-04-30 18:02:40 -0400

    feat: added get_or_create_user function, where it finds the user through github id, and returns the user if not found then create and add to db

--- app/database/crud.py
+++ app/database/crud.py
@@ -1,4 +1,18 @@
 from fastapi import HTTPException
 from sqlalchemy.orm import Session
-from app.database import models
+from . import models
 
+# CRUD operations for the database models
+# This function retrieves a user by their GitHub ID, or creates a new user if one does not exist
+def get_or_create_user(db: Session, github_id: int, user_name: str, email: str):
+    user = db.query(models.Users).filter(models.Users.github_id == github_id).first()
+    
+    if user:
+        return user
+    new_user = models.Users(github_id=github_id, user_name=user_name, email=email)
+    
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    
+    return new_user
