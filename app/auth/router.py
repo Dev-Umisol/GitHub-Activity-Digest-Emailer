34d58552a1ec1fe4e00e06e54dd63077cf322af3
Dev-Umisol <148490822+Dev-Umisol@users.noreply.github.com>
2026-05-09 19:38:21 -0400

    feat: added safety check in event client id or secret is missing from env

--- app/auth/router.py
+++ app/auth/router.py
@@ -8,3 +8,6 @@ import os
 load_dotenv()
 GITHUB_CLIENT_ID = os.getenv("GITHUB_CLIENT_ID")
 GITHUB_CLIENT_SECRET = os.getenv("GITHUB_CLIENT_SECRET")
+
+if GITHUB_CLIENT_ID is None or GITHUB_CLIENT_SECRET is None:
+    raise ValueError("GITHUB_CLIENT_ID or GITHUB_CLIENT_SECRET must be set in the environment variables.")
